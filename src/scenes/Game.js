5d7f89263ded74b7c89ddc89a4ff9c68c0a66eb5
kryonicz <brandanbok@hotmail.com>
2025-06-23 10:05:10 +0800

    cp3a

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,10 +9,21 @@ class Game extends Phaser.Scene {
 
     preload() {
         this.load.spritesheet("dino", "public/assets/dino-run.png", {frameWidth: 88, frameHeight: 94});
+
+        this.load.image("ground", "public/assets/ground.png");
+        this.load.image("cloud", "public/assets/cloud.png")
     }
 
     create() {
         this.physics.add.sprite(200, 200, "dino").setOrigin(0);
+
+        this.add.image(100, 280, "ground").setOrigin(0);
+
+        this.add.image(200, 100, "cloud").setOrigin(0);
+
+        this.add.image(300, 130, "cloud").setOrigin(0);
+        
+        this.add.image(450, 90, "cloud").setOrigin(0);
     }
 
     update() {
